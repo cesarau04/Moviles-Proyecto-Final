ff512b1a065c8ada9bee26ad8ee28235b5e1035a
Fernando Partida <ferpart@hotmail.com>
2019-02-27 10:25:46 -0600

    enterprise and user dashboards added

---
+++ app/src/main/java/mx/tec/proyectofinal/userDashboard.java
@@ -0,0 +1,13 @@
+package mx.tec.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class userDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
