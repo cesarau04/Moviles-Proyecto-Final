69cc30cdbd88f339443736cbcd351aac86e466cd
Fernando Partida <ferpart@hotmail.com>
2019-02-27 11:59:43 -0600

    CreateJob and enterprise Dashboard done (mostly)

--- app/src/main/java/mx/tec/proyectofinal/createJob.java
+++ app/src/main/java/mx/tec/proyectofinal/createJob.java
@@ -1,13 +1,34 @@
 package mx.tec.proyectofinal;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class createJob extends AppCompatActivity {
+
+    private EditText jobTitle, jobDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_job);
+
+        jobTitle=findViewById(R.id.jobTitle);
+        jobDescription=findViewById(R.id.jobDescription);
+    }
+
+    public void addJob(View v){
+        /*
+        añadir a base de datos usando
+        jobTitle.getText().toString();
+        jobDescription.getText().toString();
+         */
+
+        Intent intent = new Intent();
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/mx/tec/proyectofinal/enterpDashboard.java
+++ app/src/main/java/mx/tec/proyectofinal/enterpDashboard.java
@@ -1,13 +1,18 @@
 package mx.tec.proyectofinal;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class enterpDashboard extends AppCompatActivity {
+
+    private static final int ENTER_DASHBOAR_CODE=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +21,16 @@ public class enterpDashboard extends AppCompatActivity {
 
     }
 
+    public void jobCreator(View v){
+        Intent intent = new Intent(enterpDashboard.this, createJob.class);
+        startActivityForResult(intent, ENTER_DASHBOAR_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        if (requestCode==ENTER_DASHBOAR_CODE) {
+            if (resultCode == Activity.RESULT_OK)
+                Toast.makeText(this, "Job Added Successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
